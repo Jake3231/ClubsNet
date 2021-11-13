a99c88818058197ed161d781aaf7ce839b3fd229
Jake Spann <jake323@comcast.net>
2021-11-13 12:31:22 -0600

    Initial Commit

---
+++ Shared/MOCK_HackUTDVIIIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MOCK_HackUTDVIIIApp.swift
+//  Shared
+//
+//  Created by Jake Spann on 11/13/21.
+//
+
+import SwiftUI
+
+@main
+struct MOCK_HackUTDVIIIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
